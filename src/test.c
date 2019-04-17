917f6ce53c0fd532c93bc8bce7e5087e1bb8a48e
cddx <zeeman2003@hotmail.com>
2019-04-17 18:22:04 +0800

    make a clone on thinkpad

--- src/test.c
+++ src/test.c
@@ -1,4 +1,5 @@
 #include <REG52.H>
+#include"vstrick.h"
 extern void func(void);
 extern void f1(void);
 extern int x;
@@ -7,6 +8,7 @@ void main(void)
 	x = 10;
 	func();
 	f1();
+	P1 ^= 1;
 	while (1)
 	{
 		//P1 ^= 0x01; 				/* Toggle P1.0 each time we print */
